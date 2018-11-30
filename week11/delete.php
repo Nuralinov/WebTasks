b75ac02b1000b6ade3a2e06c5871c9d4e451f5ee
Nuralinov <43118153+Nuralinov@users.noreply.github.com>
2018-11-30 23:58:45 +0600

    Add files via upload

---
+++ week11/delete.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "alikhan";
+$password = "1234";
+$db = "task11";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $admin = $_COOKIE['admin'];
+    if($admin == 1){
+        $sql = "DELETE FROM `cars` WHERE id = ".$id."";
+        mysqli_query($conn,$sql);
+        echo 'deleted';
+    }
+    else{
+        echo 'u cant' . '<br>';
+        echo 'ur admin index is ' . $admin;
+    }
+}
+mysqli_close($con);
+?>
